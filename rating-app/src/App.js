2fea25bfe2254ca181e4b6b1def19c110b82228f
Shruti Nandan <shrutinandan31@gmail.com>
2019-04-07 18:34:13 +0530

    removed consoles

--- rating-app/src/App.js
+++ rating-app/src/App.js
@@ -86,12 +86,13 @@ const onChange = (ev)  => {
       useEffect(() => {
       // This gets called after every render, by default
       // (the first one, and every one after that)
-        console.log("render!syy",rating);
+       // console.log("render!syyn",rating);
   
       // If you want to implement componentWillUnmount,
       // return a function from here, and React will call
       // it prior to unmounting.
-      return () => console.log("unmounting...", rating);
+      //return () => 
+      //console.log("unmounting...", rating);
     });
   
   
@@ -99,8 +100,6 @@ const onChange = (ev)  => {
     
   return (
     <div>
-       <p>You clicked {rating.rating} times</p>
- 
        <div className="d-flex justify-content-between">
        <div style={{background: "grey", width:300, height: 700 }}></div>
        <div className= "p-4"style={{background: "", width:1000}}>
